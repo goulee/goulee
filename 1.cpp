ef098e7c8ac3a119e13f435d3d2449b7f5aaf6b6
goulee <3312669435@qq.com>
2022-04-15 14:57:24 +0800

    Some changes.

--- 1.cpp
+++ 1.cpp
@@ -8,8 +8,3 @@ int main()
 {
   std::cout << "Enter numeric values separated by spaces and enter Ctrl+Z to end:" << std::endl;
 
-  std::cout << "\nThe sum of the values you entered is "
-    << std::accumulate(std::istream_iterator<double>(std::cin),
-    std::istream_iterator<double>(), 0.0)
-    << std::endl;
-}
